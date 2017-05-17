28efdabd4d38c93fefb3f358399caf6e9f498aad
aurex77 <jouanisson.aurelien@gmail.com>
2017-05-17 16:00:08 +0200

    test commit

--- application/controllers/UserController.php
+++ application/controllers/UserController.php
@@ -3,15 +3,15 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class UserController extends CI_Controller {
 
-	public function __construct() {
-    parent::__construct();
+    public function __construct() {
+        parent::__construct();
 
-		$this->load->model('user_model', 'user');
-  }
+        $this->load->model('user_model', 'user');
+    }
 
-	public function index() {
-		$this->load->view('index');
-	}
+    public function index() {
+        $this->load->view('index');
+    }
 
     public function login()
     {
